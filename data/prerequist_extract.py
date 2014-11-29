3f62c5fc29ff4e54c432706d51d18d63bfc9c460
chuanrui <chuanrui@user3-17-1.wireless.utoronto.ca>
2014-11-29 15:54:15 -0500

    added express nodejs backend

--- data/prerequist_extract.py
+++ data/prerequist_extract.py
@@ -1,22 +1,40 @@
 from pymongo import MongoClient
 import re
-#log in, select db and collections
-client = MongoClient('mongodb://russ:Li199310112@ds045077.mongolab.com:45077/courseplanner')
-db = client['courseplanner']
-courses = db['coursedata']
-
-
-for course in courses.find():
-	if 'prerequisite' in course:
-		print course['prerequisite']
-
 
 class Extractor:
 	def __init__(self,prerequisite):
 		self.pre = prerequisite
 
 
-	def find_course_code(code):
+	def toCourseCodeArray(self):
+		courseCode=[]
+		for pre in re.finditer("\w\w\w\d\d\d(\w\d)?",self.pre,0):
+			courseCode.append(pre.group(0))
+		return courseCode
 
-		return 
+	
+
+
+
+
+#log in, select db and collections
+client = MongoClient('mongodb://russ:Li199310112@ds045077.mongolab.com:45077/courseplanner')
+db = client['courseplanner']
+courses = db['coursedata']
+
+#from prerequisite, for each course code found in prerequisite string, output to an array
+#and put in prerequiste_codes
+'''for course in courses.find():
+	if 'prerequisite' in course:
+		ex = Extractor(course['prerequisite'])
+		preCourseCode = ex.toCourseCodeArray()
+		courses.update({"_id":course["_id"]},{"prerequisite_codes":preCourseCode},True)
+'''
+
+#read out prerequisite_codes
+for course in courses.find():
+		if 'prerequisite_codes' in course:
+			print course
+			
+
 
---
+++ server/app/models/course.js
@@ -0,0 +1,29 @@
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var CourseSchema   = new Schema({
+	breadth: String,
+    exclusion: String,
+    long_description: String,
+    short_description: String,
+    distribution: String,
+    course_code: String,
+    Department: String,
+    Division: String,
+    Level: String,
+    session_data: [
+        {
+            Term: String,
+            session_type: String,
+            Last updated: Date,
+            session_code: String,
+            Session: String,
+            Campus: String
+        },
+    ],
+    Title: String
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
+
